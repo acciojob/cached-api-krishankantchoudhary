7173f23f7f3a5ba24181b930ff35118894c6dfd3
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-12-19 15:10:35 +0530

    render

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,68 @@
 
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import './../styles/App.css';
+import Loading from "./Loading";
 
 const App = () => {
+  const[posts,setPosts]=useState([]);
+  const[userId,setUserId]=useState("");
+  const[loading,setLoading]=useState(true);
+
+  useEffect(()=>{
+    // const fetchPostdata = async()=>{
+    //   setLoading(true)
+    //   let url="https://jsonplaceholder.typicode.com/posts";
+
+    //   if(userId!==""){
+    //     url=url+`?userId=${userId}`;
+    //   }
+    //   let res=await fetch(url);
+    //   let data=await res.json();
+    //   setPosts(data);
+    //   setLoading(false);
+    // }
+    // fetchPostdata();
+    setLoading(true);
+
+  let url = "https://jsonplaceholder.typicode.com/posts";
+
+  if (userId !== "") {
+    url += `?userId=${userId}`;
+  }
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      setPosts(data);
+      setLoading(false);
+      setUserId("");
+    });
+  },[userId])
+
+  const cachedPosts=useMemo(()=>{
+    return posts;
+  },[posts]);
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input
+        type="text"
+        value={userId}
+        onChange={(e)=>setUserId(e.target.value)}
+        />
+
+        {loading?
+        (<Loading/>):
+        (<ul>
+          {cachedPosts.map((post)=>(
+            <li key={post.id}>
+              <strong>{post.title}</strong><br/>
+              <span>{post.body}</span>
+            </li>
+          ))}
+        </ul>)
+        }   
     </div>
   )
 }
